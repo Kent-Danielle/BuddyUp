b12ccdd4f8e01d0265c153c78fc385306e494eb6
Kent-Danielle <kconcengco@my.bcit.ca>
2022-05-05 16:16:57 -0700

    Fixed minor bug with the responsive table

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -20,42 +20,47 @@ searchButton.addEventListener("click", (event) => {
 		.then(function (response) {
 			return response.text();
 		})
-		.then(async function (result) {
+		.then(function (result) {
 			let table = document.getElementById("tableBody");
-			table.innerHTML = await result;
-			createListener();
-		});
+			table.innerHTML = result;
+		})
 });
 
 createListener();
-function createListener(){
-	console.log("hi")
+function createListener() {
+	console.log("hi");
 	for (let i = 0; i < expandBtn.length; i++) {
 		expandBtn[i].addEventListener("click", (event) => {
-			if (document.getElementById("nested-table-" + (i+1)).style.display != "block") {
-				document.getElementById("nested-table-" + (i+1)).style.display = "block";
+			if (
+				document.getElementById("nested-table-" + (i + 1)).style.display !=
+				"block"
+			) {
+				document.getElementById("info").style.display = "table-row";
+				document.getElementById("nested-table-" + (i + 1)).style.display =
+					"block";
 				expandBtn[i].innerHTML = '<i class="fa-solid fa-circle-minus"></i>';
 			} else {
-				document.getElementById("nested-table-" + (i+1)).style.display = "none";
+				document.getElementById("info").style.display = "none";
+				document.getElementById("nested-table-" + (i + 1)).style.display =
+					"none";
 				expandBtn[i].innerHTML = '<i class="fa-solid fa-circle-plus"></i>';
 			}
 		});
 	}
 }
 
+// function checkMedia(x) {
+// 	if (x.matches) {
+// 		// If media query matches
+// 		nestedTable.style.display = "none";
 
-function checkMedia(x) {
-	if (x.matches) {
-		// If media query matches
-		nestedTable.style.display = "none";
+// 		if (minus.style.display != "none") {
+// 			minus.style.display = "none";
+// 			plus.style.display = "block";
+// 		}
+// 	}
+// }
 
-		if (minus.style.display != "none") {
-			minus.style.display = "none";
-			plus.style.display = "block";
-		}
-	}
-}
-
-var x = window.matchMedia("(min-width: 855px)");
-checkMedia(x); // Call listener function at run time
-x.addListener(checkMedia); // Attach listener function on state changes
+// var x = window.matchMedia("(min-width: 855px)");
+// checkMedia(x); // Call listener function at run time
+// x.addListener(checkMedia); // Attach listener function on state changes
