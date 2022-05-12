1ce4d1f0b52b49f3623dfd2cd7e1a1bf0afadc60
Kent-Danielle <kconcengco@my.bcit.ca>
2022-05-11 18:00:10 -0700

    Added character counter for the textarea

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -170,6 +170,21 @@ function createDeleteListener() {
 	});
 }
 
+const textarea = document.querySelector("textarea");
+
+textarea.addEventListener("input", ({ currentTarget: target }) => {
+	const maxLength = target.getAttribute("maxlength");
+	const currentLength = target.value.length;
+
+	if (currentLength >= maxLength) {
+		return (document.getElementById("textarea_remaining_char").innerText =
+			"You have reached the maximum number of characters.");
+	}
+
+	document.getElementById("textarea_remaining_char").innerText =
+		maxLength - currentLength + " characters left";
+});
+
 /**
  * Scripts for the modal popup for add user function
  */
