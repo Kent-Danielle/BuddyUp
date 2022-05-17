e991a265b659cc74963daa872732117639886aa0
Tommy Nguyen <tommylulxdd@gmail.com>
2022-05-17 14:26:40 -0700

    Chat room schema

---
+++ models/chat-room.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const chatroomSchema = new mongoose.Schema({
+
+    user1: {
+        type: String,
+        required: true
+    },
+    user2: {
+        type: String,
+        required: true
+    },
+    filters: {
+        type: Array,
+        required: true
+    }
+    
+})
+
+module.exports = mongoose.model('chat-room', chatroomSchema);
